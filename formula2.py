778df81f0f60860d28e6b2e4a754cd7aac016eb7
tilapiamujair <mochacinot@gmail.com>
2025-02-25 07:17:32 +0000

    Submit

---
+++ formula2.py
@@ -0,0 +1,5 @@
+#comment here
+distance = float(input("enter the distance(in meters):"))
+time = float(input("enter the time(in seconds):"))
+velocity = distance / time
+print(f"the velocity is {velocity:.2f} m/s.")
